036dcb6429d1f8618d655515aae353715e9a8474
Alex Tarana <tarana.alex@gmail.com>
2022-09-14 10:38:04 +0200

    Built web
    Branch: master
    Commit: 051d21202a13dd35071968caa293dd4acebd3fbb (dirty)
    
    package:peanut 4.3.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "b87e3bfbea956b6eab4c0cd7328e1d76",
-"index.html": "daa5c3e23e48ddec2a11ed26a52f6346",
-"/": "daa5c3e23e48ddec2a11ed26a52f6346",
-"main.dart.js": "fa51bf458adf6fece41483e64f014b59",
+  "version.json": "8d3e797499ab33035fd1c4f904df77e7",
+"index.html": "5124400ef7aa3df118507628f6f6bd4c",
+"/": "5124400ef7aa3df118507628f6f6bd4c",
+"main.dart.js": "4e4373249cb0c1dc9654b6d9641e53ed",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
